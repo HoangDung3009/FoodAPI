380163e807f44a90d2d25a311b9f5e8e996b2b8e
HoangDung3009 <hoangdung123asd@gmail.com>
2022-02-08 20:24:24 +0700

    add Food and Customer services

---
+++ FoodAPI/Services/CustomerService.cs
@@ -0,0 +1,47 @@
+﻿using FoodAPI.Interfaces;
+using FoodAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodAPI.Services
+{
+    public class CustomerService : ICustomer
+    {
+        private readonly FoodDBContext _context;
+        public CustomerService(FoodDBContext context)
+        {
+            this._context = context;
+        }
+        public void CreateCustomer(Cusomter cusomter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteCustomer(Cusomter customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Cusomter> GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Cusomter GetCustomerById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateCustomer(Cusomter customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FoodAPI/Services/FoodItemService.cs
@@ -0,0 +1,47 @@
+﻿using FoodAPI.Interfaces;
+using FoodAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FoodAPI.Services
+{
+    public class FoodItemService : IFoodItem
+    {
+        private readonly FoodDBContext _context;
+        public FoodItemService(FoodDBContext context)
+        {
+            this._context = context;
+        }
+        public void CreateFoodItem(FoodItem foodItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFoodItem(FoodItem foodItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<FoodItem> GetAllFoodItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public FoodItem GetFoodItemsById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateFoodItem(FoodItem foodItem)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
